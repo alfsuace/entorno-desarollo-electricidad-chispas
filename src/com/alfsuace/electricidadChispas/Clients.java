48fa6867696b85eb1577ba3f7af556359b294028
alfsuace <115726503+alfsuace@users.noreply.github.com>
2022-11-21 17:38:26 +0100

    UML añadido a assets y creación de las dos interfaces

---
+++ src/com/alfsuace/electricidadChispas/Clients.java
@@ -0,0 +1,18 @@
+package com.alfsuace.electricidadChispas;
+
+public interface Clients {
+    public void setClientCode();
+    public String getClientCode();
+    public void setId();
+    public String getId();
+    public void setMailingAddress();
+    public String getMailingAddress();
+    public void setTown();
+    public String getTown();
+    public void setProvince();
+    public String getProvince();
+    public void setEmail();
+    public String getEmail();
+    public void setPhoneNumber();
+    public String getPhoneNumber();
+}
---
+++ src/com/alfsuace/electricidadChispas/sale.java
@@ -0,0 +1,13 @@
+package com.alfsuace.electricidadChispas;
+
+public interface sale {
+    public void setCode();
+    public String getCode();
+    public void setName();
+    public String getName();
+    public void setPrice();
+    public Integer getPrice();
+    public void setVatType();
+    public Integer getVatType();
+
+}
